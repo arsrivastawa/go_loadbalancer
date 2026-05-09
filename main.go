1526e4b460c59a8214a3ec4d19a103df8f46df77
arsrivastawa <arsrivastawa2710@gmail.com>
2026-05-09 11:13:13 +0530

    added: struct for server and server pool

--- main.go
+++ main.go
@@ -5,7 +5,52 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync"
 )
+
+const (
+	Attempts int = 0
+	Retry    int = 0
+)
+
+type Server struct {
+	URL          string
+	Alive        bool
+	mux          sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
+}
+
+func (s *Server) isAlive() bool {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
+	return s.Alive
+}
+
+func (s *Server) setAlive(alive bool) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.Alive = alive
+}
+
+type ServerPool struct {
+	servers []*Server
+	current uint64
+}
+
+func (s *ServerPool) AddServer(server *Server) {
+	s.servers = append(s.servers, server)
+}
+
+func (s *ServerPool) MarkBackendStatus(backendUrl *url.URL, alive bool) {
+	for _, b := range s.servers {
+		if b.URL == backendUrl.String() {
+			b.setAlive(alive)
+			break
+		}
+	}
+}
 
 func requestHandler(w http.ResponseWriter, req *http.Request) {
 	id := req.URL.Query().Get("id")
